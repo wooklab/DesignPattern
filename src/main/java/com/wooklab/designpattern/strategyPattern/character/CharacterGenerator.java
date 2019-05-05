229a31a8dfff6ec3abac6d7612878cfd39bd94d8
inwook9 <inwook9@gmail.com>
2019-05-06 00:02:14 +0900

    전략패턴 클래스명 수정

--- src/main/java/com/wooklab/designpattern/strategyPattern/character/CharacterGenerator.java
+++ src/main/java/com/wooklab/designpattern/strategyPattern/character/CharacterGenerator.java
@@ -7,11 +7,11 @@ import com.wooklab.designpattern.strategypattern.character.type.impl.BowAndArrow
 import com.wooklab.designpattern.strategypattern.character.type.impl.KnifeBehavior;
 import com.wooklab.designpattern.strategypattern.character.type.impl.SwordBehavior;
 
-public class CharactorGenerator {
+public class CharacterGenerator {
     public static void main(String[] args) {
         // 캐릭터 왕
         Character characterKing = new King();                   // Character 추상클래스 변수를 구현/확장 클래스로 인스턴스를 생성
-        characterKing.fight();                                  // Character 추상클래스를 상속받은 구현클래스에서 메소드 구현
+        characterKing.fight();                                  // Character 추상클래스를 상속받은 구현클래스에서 메소드 구현(구현된 메소드)
         characterKing.performFight();                           // Character 추상클래스에서 공통으로 사용되는 메소드
         characterKing.setWeaponBehavior(new KnifeBehavior());   // Character 추상클래스에서 공통으로 사용되는 메소드, WeaponBehavior 구현체를 SET(변경될 부분을 캡슐화)
         characterKing.performFight();
